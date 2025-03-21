ae22a6d7e74e3a55c085589900712d78a750fd0f
Brian LaBarbera <labarberabrian00@gmail.com>
2025-03-21 14:37:58 -0400

    home.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
+import Home from "./components/Home";
 
 const App = () => {
   return (
-    <div>
-      
+    <div className="h-screen w-screen overflow-y-scroll scroll-smooth snap-y snap-mandatory">
+      <section className="snap-start">
+        <Home />
+      </section>
+      <section className="h-screen w-full bg-gray-900 flex items-center justify-center text-white snap-start">
+        <h1 className="text-4xl font-bold">About Me</h1>
+      </section>
+      <section className="h-screen w-full bg-gray-800 flex items-center justify-center text-white snap-start">
+        <h1 className="text-4xl font-bold">Projects</h1>
+      </section>
+      <section className="h-screen w-full bg-gray-700 flex items-center justify-center text-white snap-start">
+        <h1 className="text-4xl font-bold">Contact Me</h1>
+      </section>
     </div>
   )
 }
---
+++ src/components/Home.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Home = () => {
+
+return (
+    <div className="h-screen w-full flex flex-col items-center justify-center bg-black text-white">
+      <h1 className="text-5xl font-bold">
+      <span className="text-purple-400">SOFTWARE</span> DEVELOPER
+      </h1>
+      <p className="mt-4 text-gray-300">
+      Hi! I'm <span className="font-bold">Brian</span>, a passionate software developer eager to create stunning web solutions.
+      </p>
+    </div>
+ )
+}
+
+export default Home
